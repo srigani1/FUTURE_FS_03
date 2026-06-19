ccc4d806999ac1595181f7bb82ee6f3e24bf6539
srigani1 <sireshabandi@gmail.com>
2026-06-19 09:38:33 +0530

    Fix image paths for GitHub Pages

--- src/components/Gallery.jsx
+++ src/components/Gallery.jsx
@@ -10,10 +10,10 @@ function Gallery() {
         </h2>
 
         <div className="gallery-grid">
-          <img src="/images/interior.png" alt="Restaurant" />
-          <img src="/images/table.png" alt="Dining Area" />
-          <img src="/images/biryani.png" alt="Food" />
-          <img src="/images/interior.png" alt="Interior" />
+          <img src="images/interior.png" alt="Restaurant" />
+          <img src="images/table.png" alt="Dining Area" />
+          <img src="images/biryani.png" alt="Food" />
+          <img src="images/interior.png" alt="Interior" />
         </div>
 
       </div>
